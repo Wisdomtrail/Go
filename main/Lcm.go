1401ddd1bde410c7a955a23f89af6c843bad1e30
sunepa01 <117924550+sunepa01@users.noreply.github.com>
2023-05-25 20:31:42 +0100

    first commit

---
+++ main/Lcm.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 = 100
+	fmt.Println(num1)
+	var array = [9]int{}
+	fmt.Println(array)
+}
---
+++ main/Person.go
@@ -0,0 +1,19 @@
+package main
+
+import "strconv"
+
+func main() {
+	person := new(Person)
+	println(person.getPersonInformation())
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) getPersonInformation() string {
+	p.Age = 45
+	p.Name = "sunday"
+	return p.Name + " " + strconv.Itoa(p.Age)
+}
